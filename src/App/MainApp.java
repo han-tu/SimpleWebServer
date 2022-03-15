442640e410068e26679f1db4c513a1d1703ef258
SalmanDA <alfariqsalman@gmail.com>
2022-03-15 13:18:34 +0700

    req handling function

--- src/App/MainApp.java
+++ src/App/MainApp.java
@@ -4,6 +4,9 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -52,28 +55,100 @@ public class MainApp {
         }
 	}
 	
-//	public static String generateDirectoryPage(List<String> listDir) {
-//		//
-//	}
-//	
-//	public static String getListDir() {
-//		//
-//	}
-//	
-//	public static String generateResponseHeader(int statusCode, String statusMsg, String contentType, int contentLength) {
-//		//
-//	}
-//	
-//	public static String getFileContent() {
-//		//
-//	}
-//	
-//	public static void parseReqMethod(String reqHeader) {
-//		//
-//	}
-//	
-//	public static void parseReqPath(String reqHeader) {
-//		//
-//	}
+	public static String generateDirectoryPage(List<String> listDir) {
+		//
+	}
+	
+	public static String getListDir() {
+		//
+	}
+	
+	public static String generateResponseHeader(int statusCode, String statusMsg, String contentType, int contentLength) {
+		//
+	}
+	
+	public static String getFileContent() {
+		//
+	}
+	
+	public static String getReqHeader(String req) {
+		//
+		String header = req.split("\r\n\r\n")[0];
+        return header;
+	}
+	
+	public static void parseReqPath(String reqHeader) {
+		//
+		String pattern = "GET ([^\\s]+)";
+		Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(reqHeader);
+        if (m.find()) {
+            reqPath = m.group(1);
+            reqPath = reqPath.substring(1);
+        }
+        if (reqPath.equals("\\")) {
+        	reqPath = "";
+        }
+ 
+        try {
+			reqPath = URLDecoder.decode(reqPath, StandardCharsets.UTF_8.name());
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+ 
+	public static void parseReqMethod(String reqHeader) {
+		//
+		String pattern = "([^\\s]+)";
+		Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(reqHeader);
+        if (m.find()) {
+            reqMethod = m.group(1);
+        }
+	}
+ 
+	public static void parseHost(String reqHeader) {
+		//
+		String host = "";
+		String pattern = "Host: ([^\n]+)";
+		Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(reqHeader);
+        if (m.find()) {
+            host = m.group(1);
+        }
+ 
+        // Ambil config rootdir virtual host
+        if (!host.equals("localhost")) {
+        	FileInputStream fis;
+        	BufferedInputStream bis;
+        	String tmp = "";
+        	try {
+        		fis = new FileInputStream("config.txt");
+        		bis = new BufferedInputStream(fis);
+ 
+        		byte[] c;
+        		c = new byte[bis.available()];
+        		bis.read(c);
+        		tmp = new String(c);
+ 
+        		bis.close();
+        		fis.close();
+        	} catch (FileNotFoundException e) {
+        		e.printStackTrace();
+        	} catch (IOException e) {
+        		e.printStackTrace();
+        	}
+ 
+        	pattern = "VirtualHost: " + host + " ([^\n]+)";
+        	r = Pattern.compile(pattern);
+        	m = r.matcher(tmp);
+        	if (m.find()) {
+        		rootDir = m.group(1);
+        	}
+        }
+	}
+	
+	
 	
 }
